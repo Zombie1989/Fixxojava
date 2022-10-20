abe6eb6af3411704dc521d09a8e646d0f789acd7
Daniel Grahn <daniel_grahn@live.se>
2022-10-20 17:14:51 +0200

    startade med att göra sidan responsiv

--- src/sections/MainMenuSection.js
+++ src/sections/MainMenuSection.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import MenuIcon from '../components/MenuIcon'
 
 const MainMenuSection = () => {
+  const [ showMenu, setShowMenu ] = useState(true)
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu)
+  }
+
   return (
     <nav className="header container">
       <div className="header-logo">
-        <h1>Fixxo.</h1>
+        Fixxo.
       </div>
-      <div className="header-menu">
+      <div className={`header-menu ${ !showMenu ? "d-grid" : "" }`}>
         <NavLink className="header-link" to="/" end>Home</NavLink>
         <NavLink className="header-link" to="/categories" end>Categories</NavLink>
         <NavLink className="header-link" to="/products" end>Products</NavLink>
@@ -16,9 +22,11 @@ const MainMenuSection = () => {
       </div>
       <div className="menu-icons">
         <MenuIcon link="/search" icon="fa-regular fa-magnifying-glass"/>
-        <MenuIcon link="/compare" icon="fa-regular fa-code-compare"/>
-        <MenuIcon quantity="3" link="/wishlist" icon="fa-regular fa-heart"/>
-        <MenuIcon quantity="7" link="/shoppingcart" icon="fa-regular fa-bag-shopping"/> 
+        <MenuIcon hideOnMobile={true} link="/compare" icon="fa-regular fa-code-compare"/>
+        <MenuIcon hideOnMobile={true} quantity="3" link="/wishlist" icon="fa-regular fa-heart"/>
+        <MenuIcon quantity="7" link="/shoppingcart" icon="fa-regular fa-bag-shopping"/>
+
+        <button onClick={toggleMenu} className="menu-icon btn-menu-icon"><i className="fa-regular fa-bars"></i></button> 
       </div>
     </nav>
   )
